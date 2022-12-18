25411f440389b4d42d68f70271dff2ea01342cc2
Viktor Larin <Larinv1983@mail.ru>
2022-12-18 12:47:36 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const columns =document.querySelectorAll('.column');
+
+function generateRandomColors() {
+	const hexCode = '0123456789ABCDEF';
+	let color ='';
+	for (let i=0; i<6; i++) {
+		color += hexCode[Math.floor(Math.random() * hexCode.length)];
+	}
+	return '#'+ color;
+}
+
+function RandomColors() {
+	columns.forEach((column) => {
+		const text = column.querySelector('h2');
+		const color = generateRandomColors();
+		text.textContent = color;
+		column.style.background = color;
+	});
+};
+
+RandomColors();
