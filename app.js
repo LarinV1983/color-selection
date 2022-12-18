9d34d87b79c5c8dd958fb83b221c15cfeb14c910
Viktor Larin <Larinv1983@mail.ru>
2022-12-18 13:49:40 +0300

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const columns =document.querySelectorAll('.column');
 
+document.addEventListener('keydown', function(event) {
+	event.preventDefault();
+	if (event.code.toLowerCase() === 'space') {
+		RandomColors();
+	}
+});
+
+document.addEventListener('click', function(event) {
+	const type = event.target.dataset.type;
+	if (type === 'Lock') {
+		const node = event.target.tagName.toLowerCase() === 'i'
+		? event.target : event.target.children[0]
+		node.classList.toggle('fa-lock-open')
+		node.classList.toggle('fa-lock')
+	}
+});
+
 function generateRandomColors() {
 	const hexCode = '0123456789ABCDEF';
 	let color ='';
@@ -9,15 +26,32 @@ function generateRandomColors() {
 	return '#'+ color;
 }
 
+function copyToClick(text) {
+	return navigator.clipboard.writeText(text);
+}
+
 function RandomColors() {
 	columns.forEach((column) => {
+		const locked = column.querySelector('i').classList.contains('fa-lock');
 		const text = column.querySelector('h2');
+		const button = column.querySelector('button');
 		const color = generateRandomColors();
+
+		if (locked) {
+			return
+		}
+
 		text.textContent = color;
 		column.style.background = color;
+
+		TextColor(text, color);
+		TextColor(button, color);
 	});
 };
 
-
+function TextColor(text, color) {
+	const luminance = chroma(color).luminance();
+	text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
 
 RandomColors();
